fed5579f060425b56f2b72c2623e0773d80ab31e
linhvn0110 <linhvn0110@yahoo.com>
2018-07-18 12:17:32 +0700

    Add the /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,10 +1,49 @@
 var express = require('express');
 var app = express();
-
 var PORT = process.env.PORT || 3000; // only for Heroku port as process.env.PORT
+
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+},
+{
+    id: 2,
+    description: 'Go to market',
+    completed: false
+},
+{
+    id: 3,
+    description: 'Interview at 2 P.M',
+    completed: true
+}];
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
+});
+/**
+ * Get the list of todos
+ */
+app.get('/todos', function(req, res) {
+    res.json(todos);
+});
+
+/**
+ * Get the todo via id
+ */
+app.get('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+    todos.forEach(function (todo) {
+        if (todoId === todo.id) {
+            matchedTodo = todo;
+        }
+    });
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
 });
 
 app.listen(PORT, function () {
