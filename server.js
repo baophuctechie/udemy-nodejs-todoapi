2aace2cdc535810bbfaac88328310b0db2402e72
linhvn0110 <linhvn0110@yahoo.com>
2018-08-06 14:40:44 +0700

    Add PUT todo api

--- server.js
+++ server.js
@@ -96,6 +96,38 @@ app.delete('/todos/:id', function(req, res) {
     }
 })
 
+/**
+ * Update an todo item
+ * PUT /todos/:id
+ */
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    // Validate the completed property first
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+    // Validate the description property first
+    if (body.hasOwnProperty('description') && _.isString(body.description) 
+            && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+})
+
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
 });
