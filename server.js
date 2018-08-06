a3c4d55eeec2b15cb078f8120e81746e33f9b133
linhvn0110 <linhvn0110@yahoo.com>
2018-08-06 14:11:28 +0700

    Add the DELETE todo api

--- server.js
+++ server.js
@@ -80,6 +80,22 @@ app.post('/todos', function(req, res) {
 
 })
 
+/**
+ * Delete an todo item
+ * DELETE /todos/:id
+ */
+app.delete('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if (matchedTodo) {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    } else {
+        res.status(404).json({"error": "no todo found with that id"});
+    }
+})
+
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
 });
