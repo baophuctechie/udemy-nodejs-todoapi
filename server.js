0ba979c99dd71797016816401ec455d3d8b74fff
linhvn0110 <linhvn0110@yahoo.com>
2018-08-06 13:40:05 +0700

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require("underscore");
 
 var app = express();
 var PORT = process.env.PORT || 3000; // only for Heroku port as process.env.PORT
@@ -41,12 +42,13 @@ app.get('/todos', function(req, res) {
  */
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    /*var matchedTodo;
     todos.forEach(function (todo) {
         if (todoId === todo.id) {
             matchedTodo = todo;
         }
-    });
+    });*/
     if (matchedTodo) {
         res.json(matchedTodo);
     } else {
@@ -58,14 +60,23 @@ app.get('/todos/:id', function(req, res) {
  * POST add the todo item to the todos array
  */
 app.post('/todos', function(req, res) {
-    var body = req.body;
-    var todoItem = {
+    //var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) 
+            || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+    body.description = body.description.trim();
+    body.id = todoNextId++;
+    todos.push(body);
+    res.json(body);
+    /*var todoItem = {
         'id': todoNextId++,
         'description': body.description,
         'completed': body.completed
     };
     todos.push(todoItem);
-    res.json(todoItem);
+    res.json(todoItem);*/
 
 })
 
