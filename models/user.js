56431fd31884fb2545574411525b58317a5c48ca
linhvn0110 <linhvn0110@yahoo.com>
2018-08-15 18:33:48 +0700

    Convert email to lowercase using sequelize hooks

--- models/user.js
+++ models/user.js
@@ -15,5 +15,14 @@ module.exports = function(sequelize, DataTypes) {
                 len: [7, 100]
             }
         }
+    }, {
+        hooks: {
+            beforeValidate: function(user, options) {
+                // user.email converted into lowercase
+                if (typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase();
+                }
+            }
+        }
     });
 }
