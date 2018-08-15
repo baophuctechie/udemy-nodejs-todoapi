593e9dbfb71293cd2ca8d038c3696c06662c53aa
linhvn0110 <linhvn0110@yahoo.com>
2018-08-15 17:36:29 +0700

    Update POST /todos/:id to use sequelize

--- models/todo.js
+++ models/todo.js
@@ -4,13 +4,25 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                notEmpty: true
+                len: [1, 250],
+                isString: function(value) {
+                    if (typeof value !== 'string') {
+                        throw new Error('Description must be string.');
+                    }
+                }
             }
         },
         completed: {
             type: DataTypes.BOOLEAN,
             allowNull: false,
-            defaultValue: false
+            defaultValue: false,
+            validate: {
+                isBoolean: function(value) {
+                    if (typeof value !== 'boolean') {
+                        throw new Error('Completed must be boolean.');
+                    }
+                }
+            }
         }
     });
 };
--- server.js
+++ server.js
@@ -189,7 +189,30 @@ app.delete('/todos/:id', function(req, res) {
  */
 app.put('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body, 'description', 'completed');
+    var attributes = {};
+
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
+    }
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
+    }
+    
+    db.todo.findById(todoId).then(function(todo) {
+        if (todo) {
+            todo.update(attributes).then(function(todo) {
+                res.json(todo.toJSON());
+            }, function(e) {
+                res.status(400).json(e);
+            });
+        } else {
+            res.status(404).send();
+        }
+    }, function() {
+        res.status(500).send();
+    });
+    /*var matchedTodo = _.findWhere(todos, {id: todoId});
     var body = _.pick(req.body, 'description', 'completed');
     var validAttributes = {};
 
@@ -212,7 +235,7 @@ app.put('/todos/:id', function(req, res) {
     }
 
     _.extend(matchedTodo, validAttributes);
-    res.json(matchedTodo);
+    res.json(matchedTodo);*/
 })
 
 db.sequelize.sync().then(function() {
