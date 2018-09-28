8e0d5cb1c7a51b6b54f1c80f868896fbbce8008d
linhvn0110 <linhvn0110@yahoo.com>
2018-09-28 18:58:41 +0700

    Add the login route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -43,8 +43,25 @@ module.exports = function(sequelize, DataTypes) {
         }
     });
     // Class Method
-    userModel.associate = function(models) {
-        
+    userModel.authenticate = function(body) {
+        return new Promise(function (resolve, reject) {
+            if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+                return reject();
+            }
+
+            userModel.findOne({
+                where: {
+                    email: body.email
+                }
+            }).then(function (user) {
+                if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+                    return reject();
+                }
+                resolve(user);
+            }, function (e) {
+                reject();
+            });
+        });
     };
 
     // Instance method
